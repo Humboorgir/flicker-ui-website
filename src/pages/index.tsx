d29c383c98244916331cd54fc6c638f2f5e5d748
Humboorgir <hardcraftap@gmail.com>
2023-12-30 21:40:25 +0330

    feat(home): create basic home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
+import Button from "@/components/ui/button";
+
 const Home = () => {
-  return null;
+  return (
+    <div className="text-foreground min-h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold mb-1">The Ultimate Component Library</h1>
+      <p className="text-2xl font-bold text-foreground/60 mb-4">
+        Well-animated tailwindcss components inspired by Shadcn-UI & Material-UI
+      </p>
+      <div className="flex items-center justify-center">
+        <Button className="mr-4" size="lg">
+          View Components
+        </Button>
+        <Button variant="outline" size="lg">
+          See Docs
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
