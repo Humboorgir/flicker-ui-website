17234e0664cf6e2b9d18a18b9da11566ede73dab
Humboorgir <hardcraftap@gmail.com>
2023-12-30 21:10:30 +0330

    fix(storybook): import alias not working

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,8 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+import * as path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
@@ -11,6 +14,16 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/nextjs",
     options: {},
+  },
+  webpackFinal: (config) => {
+    config.resolve!.plugins = config.resolve!.plugins || [];
+    config.resolve!.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      })
+    );
+
+    return config;
   },
   docs: {
     autodocs: "tag",
