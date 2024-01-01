51901691fa504acffb41c6bf9ad33d3212b47862
Humboorgir <hardcraftap@gmail.com>
2024-01-01 17:34:31 +0330

    feat(_app): add 900 to Roboto weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ type AppPropsWithLayout = AppProps & {
   Component: PageWithLayout;
 };
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700", "900"] });
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
