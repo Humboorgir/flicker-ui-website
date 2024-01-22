c981f9be6d8ce7cb812a56695b8e5ceaa4ab1024
Humboorgir <hardcraftap@gmail.com>
2024-01-22 16:50:08 +0330

    feat(home): update page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,37 @@
 import HomeLayout from "@/layouts/homeLayout";
+import Container from "@/components/ui/container";
 import Button from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center text-center justify-center max-w-[98vw] px-6 pb-12">
-      <h1 className="text-3xl md:text-6xl font-extrabold mb-1">The Ultimate Component Library</h1>
-      <p className="text-lg md:text-2xl font-bold text-foreground/60 mb-4 ">
+    <Container
+      className="w-full min-w-[1px] flex flex-col items-center justify-center
+     text-center py-12 pb-28">
+      <h1
+        className="text-3xl text-white md:text-6xl 
+        font-extrabold mb-1 z-60 transition-all duration-400 ease-out">
+        <span className="text-transparent bg-clip-text bg-gradient-to-br from-foreground to-primary">
+          Your Own
+        </span>{" "}
+        Component Library
+      </h1>
+      <p className="text-lg md:text-2xl text-foreground-light mb-4 z-10">
         Well-animated tailwindcss components inspired by Shadcn-UI & Material-UI
       </p>
+
+      {/* Button group  */}
       <div className="flex items-center justify-center">
         <Button className="mr-4 w-[180px] md:w-[280px] md:text-xl md:py-3" href="/components">
           View Components
         </Button>
-        <Button className="w-[180px] md:w-[280px] md:text-xl md:py-3" variant="outline">
+        <Button
+          className="w-[180px] md:w-[280px] md:text-xl md:py-3"
+          href="/docs/installation"
+          variant="outline">
           See Docs
         </Button>
       </div>
-    </div>
+    </Container>
   );
 };
 
