ac757fa78e2c45affd346cca973bf51a4a8b4cfc
Humboorgir <hardcraftap@gmail.com>
2024-01-23 17:22:46 +0330

    feat(home): implement responsive navbar

--- src/components/home/navbar/navbar.tsx
+++ src/components/home/navbar/navbar.tsx
@@ -1,7 +1,18 @@
 import Button from "@/components/ui/button";
+import Container from "@/components/ui/container";
 import Row from "@/components/ui/row";
+import { cn } from "@/lib/utils";
+
+import { useState } from "react";
+import { HiOutlineBars3BottomLeft as Bars } from "react-icons/hi2";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  function toggleOpen() {
+    setOpen((prev) => !prev);
+  }
+
   const links = [
     {
       title: "Home",
@@ -13,28 +24,65 @@ const Navbar = () => {
     },
     {
       title: "Components",
-      href: "/",
+      href: "/components",
     },
     {
       title: "Docs",
-      href: "/",
+      href: "/docs/installation",
     },
   ];
+
   return (
-    <Row items="center" justify="center" className="py-2">
-      {links.map((link, i) => {
-        return (
+    // TODO: add fluid prop to Container
+    <Container className="w-full">
+      <Row items="center" justify="start" className="py-2 sm:justify-center">
+        {/* mobile navbar */}
+        <div className="relative w-fit h-fit">
           <Button
-            key={i}
-            variant="link"
-            size="lg"
-            href={link.href}
-            className="text-lg text-foreground decoration-foreground">
-            {link.title}
+            onClick={toggleOpen}
+            className={cn("sm:hidden text-foreground text-5xl", open && "bg-accent")}
+            size="sm"
+            variant="ghost">
+            <Bars />
           </Button>
-        );
-      })}
-    </Row>
+          <div
+            className={cn(
+              `absolute left-0 top-full flex invisible flex-col items-center justify-center
+              duration-[130ms] ease-out opacity-0 origin-top scale-[0.85] delay-150`,
+              open && "visible opacity-100 scale-100"
+            )}>
+            {links.map((link, i) => {
+              return (
+                <Button
+                  onClick={toggleOpen}
+                  href={link.href}
+                  className="w-[120px] rounded-none text-[15px]"
+                  variant="outline"
+                  key={i}>
+                  {link.title}
+                </Button>
+              );
+            })}
+          </div>
+        </div>
+
+        {/* desktop navbar items */}
+        <div className="hidden sm:flex">
+          {links.map((link, i) => {
+            return (
+              <Button
+                key={i}
+                variant="link"
+                size="lg"
+                href={link.href}
+                className="text-lg text-foreground decoration-foreground">
+                {link.title}
+              </Button>
+            );
+          })}
+        </div>
+      </Row>
+    </Container>
   );
 };
 
