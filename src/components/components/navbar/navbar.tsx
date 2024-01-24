45917d0b002ff2c03acf107e99bc3ad84fe747f5
Humboorgir <hardcraftap@gmail.com>
2024-01-24 16:03:09 +0330

    feat(components-page): modify navbar style

--- src/components/components/navbar/navbar.tsx
+++ src/components/components/navbar/navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     },
   ];
   return (
-    <Row items="center" className="py-2 px-4 md:px-12 fixed bg-background w-full z-20">
+    <Row items="center" className="py-2 px-4 md:px-12 bg-background w-full z-20">
       <span className="font-bold text-xl mr-auto bg-gradient-to-br from-primary to-foreground/50 text-transparent bg-clip-text">
         Flicker UI
       </span>
