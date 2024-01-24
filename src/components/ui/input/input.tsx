afda1f212d74ed022bd80fed297d2f0840f01408
Humboorgir <hardcraftap@gmail.com>
2024-01-24 16:03:37 +0330

    fix(Input): set text-foreground as text color

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           className="absolute flex top-0 left-0 bottom-0 right-0 rounded-md bg-background px-3 py-2 text-sm
         ring-offset-background focus-visible:outline-none border border-ring
          disabled:cursor-not-allowed disabled:opacity-50 focus:ring-offset-3 focus:border-3
-         focus:border-primary transition-[border] placeholder:text-transparent peer"
+         focus:border-primary transition-[border] placeholder:text-transparent pee text-foreground"
           placeholder={placeholder}
           ref={ref}
           {...props}
@@ -23,7 +23,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           peer-placeholder-shown:left-[13px] peer-placeholder-shown:text-base
           peer-focus:!top-0 peer-focus:!left-[8px] peer-focus:!text-xs
            bg-background px-1 transition-all pointer-events-none translate-y-[-50%]
-        z-10 top-0 left-[8px] peer-focus:text-primary text-xs">
+        z-10 top-0 left-[8px] peer-focus:text-primary text-xs text-foreground">
           {placeholder}
         </label>
       </div>
