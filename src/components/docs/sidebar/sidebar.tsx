511e05a8a795e28df25060c1a5ea0589966535a5
Humboorgir <hardcraftap@gmail.com>
2025-01-14 21:21:29 +0330

    fix(sidebar): make items scrollable on small screens

--- src/components/docs/sidebar/sidebar.tsx
+++ src/components/docs/sidebar/sidebar.tsx
@@ -4,7 +4,8 @@ import Category from "./category";
 const Sidebar = () => {
   return (
     // TODO: replace divs with semantic html (aside, ul and li)
-    <nav className="sticky top-[84px] h-fit hidden md:flex w-[16rem] flex-col items-stretch px-4">
+    // TODO: modify scroll bar style (make it thinner)
+    <nav className="sticky top-[84px] h-screen hidden md:flex w-[16rem] flex-col items-stretch px-4 ">
       {docsPages.map((category, i) => {
         return <Category key={i} category={category} />;
       })}
