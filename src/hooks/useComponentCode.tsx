d2dd55fefed579702861c54c65510397a48f1f58
Humboorgir <hardcraftap@gmail.com>
2025-02-04 21:12:30 +0330

    refactor(api): remove ui components in /component-code

--- src/hooks/useComponentCode.tsx
+++ src/hooks/useComponentCode.tsx
@@ -1,12 +1,9 @@
 import useSWR, { type Fetcher } from "swr";
 
-function useComponentCode(component: string, previewComponentOrUI: "preview" | "ui" = "preview") {
+function useComponentCode(component: string) {
   // @ts-ignore
   const fetcher: Fetcher<{ componentCode: string }> = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    component ? `/api/component-code/${previewComponentOrUI}/${component}` : null,
-    fetcher
-  );
+  const { data, error, isLoading } = useSWR(component ? `/api/component-code/${component}` : null, fetcher);
 
   return {
     code: data?.componentCode,
