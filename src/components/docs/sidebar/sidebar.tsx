b4db0e52dd553982ae33769355c290ef8213108e
Humboorgir <hardcraftap@gmail.com>
2025-02-07 19:24:23 +0330

    feat(Sidebar): make sidebar scrollable

--- src/components/docs/sidebar/sidebar.tsx
+++ src/components/docs/sidebar/sidebar.tsx
@@ -4,8 +4,7 @@ import Category from "./category";
 const Sidebar = () => {
   return (
     // TODO: replace divs with semantic html (aside, ul and li)
-    // TODO: modify scroll bar style (make it thinner)
-    <nav className="sticky top-[84px] h-screen hidden md:flex w-[16rem] flex-col items-stretch px-4 ">
+    <nav className="sticky overflow-y-auto top-[84px] h-screen hidden md:flex w-[16rem] flex-col items-stretch px-4 ">
       {docsPages.map((category, i) => {
         return <Category key={i} category={category} />;
       })}
