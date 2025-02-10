ae69b25fda6a6a6c4103ce6b91e519f58c518c49
Humboorgir <hardcraftap@gmail.com>
2025-02-10 20:54:26 +0330

    feat(docs): change Note style

--- src/components/docs/note/note.tsx
+++ src/components/docs/note/note.tsx
@@ -7,12 +7,9 @@ type NoteProps = {
 const Note = ({ children }: NoteProps) => {
   return (
     <div
-      className="relative flex flex-col my-6 dark:bg-sky-400/10 dark:text-sky-200 p-2 rounded-md overflow-hidden
-    text-sky-700 bg-sky-400/10">
-      <div className="h-6 -translate-y-1/2 w-1/4 left-0 top-1/2 absolute bg-sky-600/40 blur-2xl" />
-      <strong
-        className="text-sky-600 dark:text-sky-300 flex mr-2 tracking-wider h-fit text-lg mb-1.5 border-b-2 border-sky-600/70 
-      dark:border-sky-300/70 pb-1 w-fit">
+      className="relative flex flex-col my-6 p-2 rounded-md overflow-hidden
+      bg-black text-white/90 dark:bg-white/90 dark:text-black">
+      <strong className="text-background flex mr-2 tracking-wider h-fit text-lg mb-1.5 border-b-2 border-white dark:border-black pb-1 w-fit">
         <InfoIcon className="mt-[4px] h-5 w-5 mr-1" /> Note
       </strong>
       {children}
