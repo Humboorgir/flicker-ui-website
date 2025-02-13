92623bf4dfe44bba762822656634bdb22958b524
Humboorgir <hardcraftap@gmail.com>
2025-02-13 21:59:05 +0330

    refactor: remove unused code

--- src/components/docs/code/code.tsx
+++ src/components/docs/code/code.tsx
@@ -4,21 +4,6 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypePrettyCode from "rehype-pretty-code";
 
-/**
- * Server Component example
- */
-
-export default async function Code({ code }: { code: string }) {
-  const highlightedCode = await highlightCode(code);
-  return (
-    <section
-      dangerouslySetInnerHTML={{
-        __html: highlightedCode,
-      }}
-    />
-  );
-}
-
 export async function highlightCode(code: string) {
   const file = await unified()
     .use(remarkParse)
