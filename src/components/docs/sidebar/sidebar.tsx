62c42a887e259e00d15c147a95b628966499a19a
Humboorgir <hardcraftap@gmail.com>
2025-02-23 19:21:42 +0330

    fix(Sidebar): height slightly larger than viewport height

--- src/components/docs/sidebar/sidebar.tsx
+++ src/components/docs/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ import Category from "./category";
 const Sidebar = () => {
   return (
     // TODO: replace divs with semantic html (aside, ul and li)
-    <nav className="sticky overflow-y-auto top-[84px] h-screen hidden md:flex w-[16rem] flex-col items-stretch px-4 ">
+    <nav className="sticky overflow-y-auto top-[84px] h-[calc(100vh-84px)] hidden md:flex w-[16rem] flex-col items-stretch px-4 ">
       {docsPages.map((category, i) => {
         return <Category key={i} category={category} />;
       })}
