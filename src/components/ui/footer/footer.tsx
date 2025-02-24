6141a1e5c72907933c6f1c7dbb8a2d14242f5d8e
Humboorgir <hardcraftap@gmail.com>
2025-02-24 20:21:43 +0330

    refactor(Footer): make text sm

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -7,13 +7,8 @@ type Props = HtmlHTMLAttributes<HTMLDivElement>;
 
 const Footer = ({ className, ...props }: Props) => {
   return (
-    <footer
-      className={cn(
-        `border-t border-ring mt-28 py-5 px-8 flex items-center`,
-        className
-      )}
-      {...props}>
-      <Typography variant="p">
+    <footer className={cn(`border-t border-ring mt-28 py-5 px-8 flex items-center`, className)} {...props}>
+      <Typography className="text-sm" variant="p">
         Copyright &copy; FlickerUI 2024 - all rights reserved
       </Typography>
     </footer>
